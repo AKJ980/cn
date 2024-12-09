168a87090805683f7622dc5234ee94db16ea8a49
AKJ980 <148428790+AKJ980@users.noreply.github.com>
2024-12-09 21:29:25 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,94 +1,62 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 #define MAX_ROUTERS 10
 #define INFINITY 9999
 
-typedef struct {
-    int cost[MAX_ROUTERS];
-    int nextHop[MAX_ROUTERS];
-} Router;
-
-Router routers[MAX_ROUTERS];
 int numRouters;
+int cost[MAX_ROUTERS][MAX_ROUTERS]; // Cost matrix
+int dist[MAX_ROUTERS][MAX_ROUTERS]; // Distance vector
+int nextHop[MAX_ROUTERS][MAX_ROUTERS]; // Next hop matrix
 
-void initializeRouter(int routerId) {
+void initializeRoutingTable() {
     for (int i = 0; i < numRouters; i++) {
-        if (i == routerId) {
-            routers[routerId].cost[i] = 0;
-            routers[routerId].nextHop[i] = i;
-        } else {
-            routers[routerId].cost[i] = INFINITY;
-            routers[routerId].nextHop[i] = -1;
+        for (int j = 0; j < numRouters; j++) {
+            dist[i][j] = cost[i][j];
+            nextHop[i][j] = (cost[i][j] < INFINITY && i != j) ? j : -1;
         }
     }
 }
 
-void printRoutingTable(int routerId) {
-    printf("Routing table for Router %d:\n", routerId);
-    for (int i = 0; i < numRouters; i++) {
-        if (routers[routerId].cost[i] != INFINITY) {
-            printf("Destination: %d, Cost: %d, Next Hop: %d\n", i, routers[routerId].cost[i], routers[routerId].nextHop[i]);
-        } else {
-            printf("Destination: %d, Cost: INFINITY\n", i);
-        }
-    }
-}
-
-void updateRoutingTable(int routerId) {
-    for (int i = 0; i < numRouters; i++) {
-        if (i != routerId) {
-            for (int j = 0; j < numRouters; j++) {
-                if (routers[routerId].cost[j] + routers[j].cost[i] < routers[routerId].cost[i]) {
-                    routers[routerId].cost[i] = routers[routerId].cost[j] + routers[j].cost[i];
-                    routers[routerId].nextHop[i] = routers[routerId].nextHop[j];
+void updateRoutingTable() {
+    for (int k = 0; k < numRouters; k++) { // Intermediate router
+        for (int i = 0; i < numRouters; i++) { // Source router
+            for (int j = 0; j < numRouters; j++) { // Destination router
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                    nextHop[i][j] = nextHop[i][k];
                 }
             }
         }
     }
 }
 
-void simulateRouting() {
+void printRoutingTable(int routerId) {
+    printf("Routing Table for Router %d:\n", routerId);
     for (int i = 0; i < numRouters; i++) {
-        updateRoutingTable(i);
+        if (dist[routerId][i] < INFINITY) {
+            printf("To: %d, Cost: %d, Next Hop: %d\n", i, dist[routerId][i], nextHop[routerId][i]);
+        } else {
+            printf("To: %d, Cost: INFINITY\n", i);
+        }
     }
 }
 
 int main() {
     printf("Enter the number of routers: ");
     scanf("%d", &numRouters);
-    
-    int costs[MAX_ROUTERS][MAX_ROUTERS];
+
     printf("Enter the cost matrix (use %d for INFINITY):\n", INFINITY);
     for (int i = 0; i < numRouters; i++) {
         for (int j = 0; j < numRouters; j++) {
-            scanf("%d", &costs[i][j]);
+            scanf("%d", &cost[i][j]);
         }
     }
 
-    for (int i = 0; i < numRouters; i++) {
-        initializeRouter(i);
-        for (int j = 0; j < numRouters; j++) {
-            if (costs[i][j] < INFINITY) {
-                routers[i].cost[j] = costs[i][j];
-                routers[i].nextHop[j] = j;
-            }
-        }
-    }
+    initializeRoutingTable();
+    updateRoutingTable();
 
-    for (int i = 0; i < numRouters; i++) {
-        printRoutingTable(i);
-    }
-
-    simulateRouting();
-
-    printf("\nUpdated Routing Tables:\n");
     for (int i = 0; i < numRouters; i++) {
         printRoutingTable(i);
     }
 
     return 0;
 }
-
